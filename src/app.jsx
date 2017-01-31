4d2edd20d792df88d81d64b1bb65ccdc0b999823
Dmitri Wolf <Dmitri.a.wolf@gmail.com>
2017-01-30 21:24:06 -0600

    app rebuild react and webpack complete

--- src/app.jsx
+++ src/app.jsx
@@ -1,24 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Header from './components/Header';
+import MessageList from './components/MessageList';
 import Footer from './components/Footer';
 
 ReactDOM.render(
-	<div>
+	<div id="app">
 	  <Header />
-	  <h1>App!</h1>
+	  <MessageList />
 	  <Footer />
 	</div>,
   document.getElementById('root')
 );
-
-// 'use strict'
-// var React = require('react')
-// console.log('loading app.jsx');
-// module.exports = React.createClass({
-//     displayName: 'HelloReact',
-//     render: function(''){
-//         return <div>Hello React</div>
-//     }
-// })
-
---
+++ src/components/MessageList.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class MessageList extends Component {
+  render() {
+    return (
+    	<ul id="messages"></ul>
+    )
+  }
+}
+
+export default MessageList; 
