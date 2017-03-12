2889fb2ce3f07306c926631e57eb65c74fba1cba
Dmitri Wolf <Dmitri.a.wolf@gmail.com>
2017-03-12 00:48:13 -0600

    removing controls that don't work

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,14 +3,7 @@ import React, { Component } from 'react';
 class Header extends Component {
   render() {
     return (
-    	<header>
-    		<h1>Team Manager</h1>
-    		<div className="header-controls">
-		    	<span className="header-label">Sort By: </span>
-		    	<button id="sortby-job" onClick={this.props.sortBy.bind(this)}>Job</button>
-		    	<button id="sortby-installer" onClick={this.props.sortBy.bind(this)}>Installer</button>
-		    </div>
-    	</header>
+    	<header>Team Manager</header>
     )
   }
 }
